8173a90d4536b46d0d35f7fd1a6e6a8aa0b36191
liza <vinogradova.lizaveta@gmail.com>
2023-08-08 12:56:28 +0300

    get connection and rates

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,40 @@
-package app
+package main
+
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	url := "wss://ws-feed.exchange.coinbase.com"
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer conn.Close()
+
+	subscribeMsg := []byte(`{
+        "type": "subscribe",
+        "channels": [
+            {
+                "name": "ticker",
+                "product_ids": ["BTC-USD"]
+            }
+        ]
+    }`)
+	err = conn.WriteMessage(websocket.TextMessage, subscribeMsg)
+	if err != nil {
+		log.Println("write:", err)
+		return
+	}
+
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			return
+		}
+		log.Printf("Received message type %d: %s", messageType, message)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,4 @@ go 1.20
 
 require github.com/jmoiron/sqlx v1.3.5
 
-require (
-	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.25.0 // indirect
-)
+require github.com/gorilla/websocket v1.5.0 // indirect
