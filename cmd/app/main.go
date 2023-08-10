f2dcdbff6c7a9da31f3a7e6ffc47519eb508f8b0
liza <vinogradova.lizaveta@gmail.com>
2023-08-10 14:53:29 +0300

    added service logic and mysql

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"rates-listener/internal/client/coinbase"
+	"rates-listener/internal/repo/mysql"
+	"rates-listener/internal/service"
 )
 
 func main() {
+
+	config := mysql.Config{
+		Host:     "localhost",
+		Port:     "3306",
+		Username: "user",
+		Password: "1234",
+		DBName:   "ticks",
+		SSLMode:  "disable",
+	}
+
 	url := "wss://ws-feed.exchange.coinbase.com"
-	client, _ := coinbase.NewCoinBaseClient(url)
+	client, err := coinbase.NewCoinBaseClient(url)
+	if err != nil {
+		panic(fmt.Sprintf("Couldnt create CoinBase Client: %s", err))
+	}
+	repository, err := mysql.NewTickRepository(config)
+	if err != nil {
+		panic(fmt.Sprintf("Couldnt create Repository: %s", err))
+	}
 	defer client.Conn.Close()
 
-	const batchSize = 10
-	messages := make([]coinbase.TickClientDTO, batchSize)
-
-	for {
-		coinbase.ReadBatchOfMessages(client, messages)
-	}
+	batchSize := 10
+	service.NewTickService(repository, client, batchSize).Run()
 }
