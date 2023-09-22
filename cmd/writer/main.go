5a354e4cb1a66fdad2e74b72bd619d2a76d2a784
liza <vinogradova.lizaveta@gmail.com>
2023-09-22 11:58:14 +0300

    added common file with settings for the mains

--- cmd/writer/main.go
+++ cmd/writer/main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
+	"rates-listener/cmd"
 	"rates-listener/internal/brocker/kafka"
 	"rates-listener/internal/client/coinbase"
 	"rates-listener/internal/service"
-
-	log "github.com/sirupsen/logrus"
 
 	"github.com/spf13/viper"
 )
 
 func main() {
-	if err := initConfig(); err != nil {
+	if err := cmd.InitConfig(); err != nil {
 		panic(fmt.Sprintf("Couldnt initialize the config file: %s", err))
 	}
-	setupLogging()
+	cmd.SetupLogging()
 
 	url := viper.GetString("coinBaseClient.url")
 	client, err := coinbase.NewCoinBaseClient(url)
@@ -29,32 +28,4 @@ func main() {
 	viper.SetDefault("service.batchSize", 1)
 	batchSize := viper.GetInt("service.batchSize")
 	service.NewTickWriterService(client, broker, batchSize).RunToKafka()
-}
-
-func initConfig() error {
-	viper.AddConfigPath("../../configs")
-	viper.SetConfigName("config")
-	viper.SetConfigType("yml")
-	return viper.ReadInConfig()
-}
-
-func setupLogging() {
-	logLevel := viper.GetString("logLevel")
-
-	switch logLevel {
-	case "debug":
-		log.SetLevel(log.DebugLevel)
-	case "info":
-		log.SetLevel(log.InfoLevel)
-	case "warning":
-		log.SetLevel(log.WarnLevel)
-	case "error":
-		log.SetLevel(log.ErrorLevel)
-	case "fatal":
-		log.SetLevel(log.FatalLevel)
-	case "panic":
-		log.SetLevel(log.PanicLevel)
-	default:
-		log.SetLevel(log.InfoLevel)
-	}
 }
